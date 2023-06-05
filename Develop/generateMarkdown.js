1c2dd13d3a01ece089584224546ef06f98a35db6
Joshua <23joshtc@gmail.com>
2023-06-05 17:53:38 -0400

    create function that returns license section of readme

--- Develop/generateMarkdown.js
+++ Develop/generateMarkdown.js
@@ -16,3 +16,14 @@ function renderLicenseBadge(license) {
     return `\n* [License](#license)\n`; 
     
   }
+    // TODO: Create a function that returns the license section of README
+  // If there is no license, return an empty string
+  function renderLicenseSection(license) {
+    if (license === 'None') {
+      return '';
+    }
+  
+    return `## License
+  
+  This project is licensed under the ${license} license.`;
+  }
