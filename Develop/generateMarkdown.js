d867a6508666e7a539c4b34b9c3f2e40bd700b65
Joshua <23joshtc@gmail.com>
2023-06-05 17:51:26 -0400

    create function that returns license badge

--- Develop/generateMarkdown.js
+++ Develop/generateMarkdown.js
@@ -7,34 +7,3 @@ function renderLicenseBadge(license) {
   return `![License](https://img.shields.io/badge/license-${encodeURIComponent(license)}-blue.svg)`;
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license === 'None'){
-    return '';
-  }
-
-  return `\n* [License](#license)\n`; 
-  
-}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (license === 'None') {
-    return '';
-  }
-
-  return `## License
-
-This project is licensed under the ${license} license.`;
-}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-  ${renderLicenseBadge(data.license)}
-`;
-}
-
-module.exports = generateMarkdown;
