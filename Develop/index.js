6a701f9e769d185961625550aeb2023c34df6506
Joshua <23joshtc@gmail.com>
2023-06-05 00:14:01 -0400

    added fs file system, inquirer, and generateMarkdown file path to js file

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,47 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('/generateMarkdown');
+
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//create object inside of array
+const questions = [
+    {
+        //title
+    },
+    {
+        //Description
+    }, 
+    {
+
+    },
+    {
+        //technologies used
+    },
+    {
+        //usage
+    },
+    {
+        //tests
+    },
+    {
+        //lisence
+    },
+    {
+        //Github
+    },
+    {
+        //Email
+    }
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    
+
 }
 
 // TODO: Create a function to initialize app
